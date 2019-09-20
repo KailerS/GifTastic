2b6c2cc600bf172b85cce3c07977c10e60cc676e
KailerS <KailerS@users.noreply.github.com>
2019-09-19 19:47:54 -0600

    added buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,12 @@
 var topic = ["Lamborghini","Ferrari", "Bentley", "Ford GT", "Bugatti", "Rolls Royce"]
+
+
+function renderButtons() {    
+    $("#buttonGarage").empty();  
+
+    for (var i = 0; i < topic.length; i++){
+      $("#buttonGarage").append(`<button data-${topic[i]}>${topic[i]}`);        
+    };
+  };
+
+  renderButtons();
