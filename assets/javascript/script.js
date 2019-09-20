ab5556bfb1964c18a05054fb9c24dfeebdc05ac5
KailerS <KailerS@users.noreply.github.com>
2019-09-19 20:29:08 -0600

    got still images to display on button click

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,44 @@
-var topic = ["Lamborghini","Ferrari", "Bentley", "Ford GT", "Bugatti", "Rolls Royce"]
+var topic = ["Lamborghini","Ferrari", "Bentley", "Ford GT", "Bugatti", "Rolls-Royce"]
 
 
 function renderButtons() {    
     $("#buttonGarage").empty();  
 
     for (var i = 0; i < topic.length; i++){
-      $("#buttonGarage").append(`<button data-${topic[i]}>${topic[i]}`);        
+      $("#buttonGarage").append(`<button data-name=${topic[i]}>${topic[i]}`);        
     };
-  };
+};
+
+$("#buttonGarage").on("click", "button", function() {
+    var carBrand = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        carBrand + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    console.log(carBrand);
+    
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var carImage = $("<img>");
+            carImage.attr("src", results[i].images.fixed_height_still.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(carImage);
+
+            $("#parkingLot").prepend(gifDiv);
+        }
+        console.log(response);
+    });
+});
 
   renderButtons();
